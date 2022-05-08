552bc7cd3a4c7b29895e0b8b5ef9f48a915e85cb
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2022-05-08 00:34:51 -0400

    ahhhh

--- compare.py
+++ compare.py
@@ -65,8 +65,9 @@ def diff_folders(new, old, type, delFolder=False):
 
     for root, dirs, files in os.walk(f"temp/{new}"):
         for name in files:
-            src = os.path.join(root, name).replace("\\", "/")
+            src = os.path.join(root, name)
             if f"temp/{new}/assets/minecraft/textures/" in src:
+                print(src)
                 dest = src.replace(new, old, 1)
 
                 if delFolder == False:
@@ -78,13 +79,13 @@ def diff_folders(new, old, type, delFolder=False):
                     deleted.append(src)
 
     for item in added:
-        save_diff(new, f"/home/runner/work/mc-texture-changes/mc-texture-changes/{type.capitalize()}s/{new}/added", item)
+        save_diff(new, f"{type.capitalize()}s/{new}/added", item)
 
     for item in changed:
-        save_diff(new, f"/home/runner/work/mc-texture-changes/mc-texture-changes/{type.capitalize()}s/{new}/changed", item)
+        save_diff(new, f"{type.capitalize()}s/{new}/changed", item)
 
     for item in deleted:
-        save_diff(new, f"/home/runner/work/mc-texture-changes/mc-texture-changes/{type.capitalize()}s/{old}/deleted", item)
+        save_diff(new, f"{type.capitalize()}s/{old}/deleted", item)
 
 
 def save_diff(base_folder, new_folder, item):
